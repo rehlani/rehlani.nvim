3bbb59c0a916aa3b5e02479c306acbc89dfdd9af
Rehan Lalani <rehanlalani@Rehans-MacBook-Air.local>
2023-05-10 21:42:50 -0700

    switch to lazyvim

--- after/plugin/line.lua
+++
@@ -1,4 +0,0 @@
-local line = require('lualine')
-
-line.setup()
-
--- init.lua
+++ init.lua
@@ -1,3 +1,2 @@
-require("basics")
-require("remaps")
-require("plugins")
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
